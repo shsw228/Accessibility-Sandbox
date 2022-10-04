9b9043ded090260610d24bd93438600c44a81fe7
shsw228 <tate.kng0228@gmail.com>
2022-10-04 10:24:33 +0900

    Initial Commit

---
+++ accessibility-sampleUITests/accessibility_sampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  accessibility_sampleUITestsLaunchTests.swift
+//  accessibility-sampleUITests
+//
+//  Created by Kengo Tate on 2022/10/04.
+//
+
+import XCTest
+
+final class accessibility_sampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
